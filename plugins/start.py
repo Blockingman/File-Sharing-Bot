c9956ff994cd092329e5f8c0e0721a4e7117c736
Blockingman <108091121+Blockingman@users.noreply.github.com>
2022-06-24 00:22:37 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -90,8 +90,8 @@ async def start_command(client: Client, message: Message):
         reply_markup = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("😊 මම", callback_data = "about"),
-                    InlineKeyboardButton("🔒 Close", callback_data = "close")
+                    InlineKeyboardButton("About ☢️", callback_data = "about"),
+                    InlineKeyboardButton("🔒 Close ", callback_data = "close")
                 ]
             ]
         )
